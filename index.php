7ecadb25f9295f1e907e6e60dc44f3a28bd24ed5
Romain GUITTEAU--GIROIRE <r.guitteau@ceso-gto.com>
2019-12-10 08:57:01 +0100

    Premier commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Configuration Mikrotik</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<main>
+			<h1>Configuration Mikrotik</h1>
+		</main>
+	</body>
+</html>
