61741905bf6fa9b229463ca6604a3ce556da4772
Romain GUITTEAU--GIROIRE <r.guitteau@ceso-gto.com>
2019-12-16 16:58:44 +0100

    Mise en page de la page web avec bootstrap

--- index.php
+++ index.php
@@ -7,104 +7,124 @@ $version = "v1.0beta"
 	<head>
 		<title>Configuration Mikrotik <?= $version ?></title>
 		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="icon" type="image/png" href="images/gto.png" />
+		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<link rel="stylesheet" href="css/style.css" />
 		<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> 
 	</head>
 	<body>
-		<main>
-			<h1><img alt="Logo GTO" src="images/gto.png" />Configuration Mikrotik <?= $version ?></h1>
-			<form action="config.php" method="post" id="formulaire">
-				<fieldset>
-					<legend>Informations de base</legend>
+		<header class="page-header">
+			<div class="col-sm-12">
+				<h1><img alt="Logo GTO" src="images/gto.png" />Configuration Mikrotik <?= $version ?></h1>
+			</div>
+		</header>
+		<div class="row">
+			<aside class="col-sm-3">
+			</aside>
+			<main class="col-sm-6">
+				<form action="config.php" method="post" id="formulaire">
+					<fieldset>
+						<legend>Informations de base</legend>
+						<p>
+							<label for="nom">Nom du routeur: </label>
+							<input type="text" name="nom" id="nom" class="form-control">
+						</p>
+						<p>
+							<label for="cesoUser">Utilisateur CESO: </label>
+							<input type="text" name="cesoUser" id="cesoUser" value="ceso" class="form-control">
+							<input type="text" name="cesoMdp" id="cesoMdp" value="isx@3009" class="form-control">
+						</p>
+						<p>
+							<label for="localUser">Utilisateur local: </label>
+							<input type="text" name="localUser" id="localUser" value="client" class="form-control">
+							<input type="text" name="localMdp" id="localMdp" value="ceso-admin" class="form-control">
+						</p>
+					</fieldset>
+					<fieldset>
+						<legend>Configuration des interfaces</legend>
+						<p>
+							<label for="modele">Modèle du routeur: </label>
+							<select name="modele" id="modele" class="form-control">
+								<option value="hex-rb750gr3">0 - hEX RB750Gr3</option>
+								<option value="rb951">1 - RB951</option>
+								<option value="rb3011-uias-rm">2 - RB3011 UiAS-RM</option>
+							</select>
+						</p>
+						<p>
+							<label for="nbLAN">Nombre de LAN: </label>
+							<input type="number" name="nbLAN" id="nbLAN" min="1" max="2" value="1" class="form-control">
+						</p>
+						<p>
+							<table class="table table-striped">
+								<!-- <caption>Assignation des interfaces</caption> -->
+								<tr>
+									<th></th>
+									<th>LAN 1</th>
+									<th>LAN 2</th>
+								</tr>
+								<?php for($i=2; $i <= 10; $i++) : ?>
+								<tr>
+									<td><?= $i ?></td>
+									<td>
+										<input type="checkbox" name="e1<?= $i ?>" id="1<?= $i ?>" class="form-control checkbox" />
+									</td>
+									<td>
+										<input type="checkbox" name="e2<?= $i ?>" id="2<?= $i ?>" class="form-control checkbox" />
+									</td>
+								</tr>
+								<?php endfor; ?>
+							</table>
+						</p>
+						<p>
+							<label for="nat">NAT: </label>
+							<input type="checkbox" name="nat" id="nat" checked  class="form-control checkbox"/>
+						</p>
+						<p>
+							<label for="SSID">SSID: </label>
+							<input type="text" name="ssid" id="ssid"  class="form-control"/>
+						</p>
+						<p>
+							<label for="psk">PSK: </label>
+							<input type="text" name="psk" id="psk"  class="form-control"/>
+						</p>
+					</fieldset>
+					<fieldset>
+						<legend>Assignation des adresses IP</legend>
+						<p>
+							<label for="ipclient">IP Client: </label>
+							<input class="ip" type="text" name="ipclient1" id="ipclient1" class="form-control" /> .
+							<input class="ip" type="text" name="ipclient2" id="ipclient2" class="form-control" /> .
+							<input class="ip" type="text" name="ipclient3" id="ipclient3" class="form-control" /> .
+							<input class="ip" type="text" name="ipclient4" id="ipclient4" class="form-control" /> /
+							<input class="ip" type="number" name="masque" id="masque" min="25" max="31" value="31"  class="form-control" />
+						</p>
+						<p>
+							<label for="ipclient">IP Gateway: </label>
+							<input class="ip" type="text" name="ipgateway1" id="ipgateway1" class="form-control" /> .
+							<input class="ip" type="text" name="ipgateway2" id="ipgateway2" class="form-control" /> .
+							<input class="ip" type="text" name="ipgateway3" id="ipgateway3" class="form-control" /> .
+							<input class="ip" type="text" name="ipgateway4" id="ipgateway4" class="form-control" />
+						</p>
+					</fieldset>
 					<p>
-						<label for="nom">Nom du routeur: </label>
-						<input type="text" name="nom" id="nom">
+						<center>
+							<button type="button" id="generer" class="btn btn-primary">
+								<span class="glyphicon glyphicon-ok-sign"></span>
+								Générer
+							</button>
+						</center>
 					</p>
-					<p>
-						<label for="cesoUser">Utilisateur CESO: </label>
-						<input type="text" name="cesoUser" id="cesoUser" value="ceso">
-						<input type="text" name="cesoMdp" id="cesoMdp" value="isx@3009">
-					</p>
-					<p>
-						<label for="localUser">Utilisateur local: </label>
-						<input type="text" name="localUser" id="localUser" value="client">
-						<input type="text" name="localMdp" id="localMdp" value="ceso-admin">
-					</p>
-				</fieldset>
-				<fieldset>
-					<legend>Configuration des interfaces</legend>
-					<p>
-						<label for="modele">Modèle du routeur: </label>
-						<select name="modele" id="modele">
-							<option value="hex-rb750gr3">0 - hEX RB750Gr3</option>
-							<option value="rb951">1 - RB951</option>
-							<option value="rb3011-uias-rm">2 - RB3011 UiAS-RM</option>
-						</select>
-					</p>
-					<p>
-						<label for="nbLAN">Nombre de LAN: </label>
-						<input type="number" name="nbLAN" id="nbLAN" min="1" max="2" value="1">
-					</p>
-					<p>
-						<table>
-							<!-- <caption>Assignation des interfaces</caption> -->
-							<tr>
-								<th></th>
-								<th>LAN 1</th>
-								<th>LAN 2</th>
-							</tr>
-							<?php for($i=2; $i <= 10; $i++) : ?>
-							<tr>
-								<td><?= $i ?></td>
-								<td>
-									<input type="checkbox" name="e1<?= $i ?>" id="1<?= $i ?>" class="lan1" />
-								</td>
-								<td>
-									<input type="checkbox" name="e2<?= $i ?>" id="2<?= $i ?>" class="lan2" />
-								</td>
-							</tr>
-							<?php endfor; ?>
-						</table>
-					</p>
-					<p>
-						<label for="nat">NAT: </label>
-						<input type="checkbox" name="nat" id="nat" checked />
-					</p>
-					<p>
-						<label for="SSID">SSID: </label>
-						<input type="text" name="ssid" id="ssid" />
-					</p>
-					<p>
-						<label for="psk">PSK: </label>
-						<input type="text" name="psk" id="psk" />
-					</p>
-				</fieldset>
-				<fieldset>
-					<legend>Assignation des adresses IP</legend>
-					<p>
-						<label for="ipclient">IP Client: </label>
-						<input class="ip" type="text" name="ipclient1" id="ipclient1" /> .
-						<input class="ip" type="text" name="ipclient2" id="ipclient2" /> .
-						<input class="ip" type="text" name="ipclient3" id="ipclient3" /> .
-						<input class="ip" type="text" name="ipclient4" id="ipclient4" /> /
-						<input class="ip" type="number" name="masque" id="masque" min="25" max="31" value="31" />
-					</p>
-					<p>
-						<label for="ipclient">IP Gateway: </label>
-						<input class="ip" type="text" name="ipgateway1" id="ipgateway1" /> .
-						<input class="ip" type="text" name="ipgateway2" id="ipgateway2" /> .
-						<input class="ip" type="text" name="ipgateway3" id="ipgateway3" /> .
-						<input class="ip" type="text" name="ipgateway4" id="ipgateway4" />
-					</p>
-				</fieldset>
-				<p>
-					<button type="button" id="generer">
-						<img src="images/valider.png" alt="valider" /> Générer
-					</button>
-				</p>
-			</form>
-		</main>
+				</form>
+			</main>
+			<aside class="col-sm-3">
+			</aside>
+		</div>
+		<footer class="row">
+        	<div class="col-sm-12">
+        		Pied de page
+        	</div>
+      	</footer>
 		<script type="text/javascript" src="js/hex-rb750gr3.js"></script>
 		<script type="text/javascript" src="js/rb951.js"></script>
 		<script type="text/javascript" src="js/rb3011-uias-rm.js"></script>
