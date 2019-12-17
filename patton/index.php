6cfb8bce395911ef4a54290de0546d80b28026ce
Romain GUITTEAU--GIROIRE <r.guitteau@ceso-gto.com>
2019-12-17 10:57:55 +0100

    Finalisation du formulaire des pattons

--- patton/index.php
+++ patton/index.php
@@ -5,7 +5,7 @@ $version = "v1.0beta"
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
-		<title>Configuration Mikrotik <?= $version ?></title>
+		<title>Configuration Patton <?= $version ?></title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="icon" type="image/png" href="../images/gto.png" />
@@ -16,7 +16,7 @@ $version = "v1.0beta"
 	<body>
 		<header class="page-header">
 			<div class="col-sm-12">
-				<h1><img alt="Logo GTO" src="../images/gto.png" />Configuration Mikrotik <?= $version ?></h1>
+				<h1><img alt="Logo GTO" src="../images/gto.png" />Configuration Patton <?= $version ?></h1>
 			</div>
 		</header>
 		<div class="row">
@@ -41,6 +41,82 @@ $version = "v1.0beta"
 					</fieldset>
 					<fieldset>
 						<legend>Paramètres IP et @IP eth0/0</legend>
+						<p>
+							<label for="ipaddress">Adresse IP: </label>
+							<input type="text" name="ipaddress" id="ipaddress" class="form-control" placeholder="Taper 'dhcp' ou l'@IP..." />
+						</p>
+						<p>
+							<label for="mask">Masque: </label>
+							<input type="text" name="mask" id="mask" class="form-control" placeholder="Pas utiliser si dhcp..." />
+						</p>
+						<p>
+							<label for="gateway">Passerelle par défaut: </label>
+							<input type="text" name="gateway" id="gateway" class="form-control" placeholder="Pas utiliser si dhcp..." />
+						</p>
+						<p>
+							<label for="dns">Serveur DNS: </label>
+							<input type="text" name="dns" id="dns" class="form-control" placeholder="Pas utiliser si dhcp..." />
+						</p>
+						<p>
+							<label for="sntp">Serveur SNTP: </label>
+							<input type="text" name="sntp" id="sntp" class="form-control" placeholder="Pas utiliser si dhcp..." />
+						</p>
+					</fieldset>
+					<fieldset>
+						<legend>Paramètres SIP</legend>
+						<p>
+							<label for="sipserverIP">Serveur SIP ou FQDN: </label>
+							<input list="sipserverlist" type="text" id="sipserverIP" name="sipserverIP" class="form-control" placeholder="Domaine ou ip registrar...">
+							<datalist id="sipserverlist">
+								<option value="sip.openvno.net">
+								<option value="sip2.openvno.net">
+								<option value="sip3.openvno.net">
+							</datalist>
+						</p>
+						<p>
+							<label for="sipserverPort">Port serveur SIP: </label>
+							<input type="text" name="sipserverPort" id="sipserverPort" class="form-control" placeholder="Port par défaut 5060..." />
+						</p>
+						<p>
+							<label for="sipserverLogin">Nom d'utilisateur SIP: </label>
+							<input type="text" name="sipserverLogin" id="sipserverLogin" class="form-control" placeholder="Login SIP..." />
+						</p>
+						<p>
+							<label for="sipserverPassword">Mot de passe SIP: </label>
+							<input type="text" name="sipserverPassword" id="sipserverPassword" class="form-control" placeholder="Password SIP..." />
+						</p>
+					</fieldset>
+					<fieldset>
+						<legend>Paramètres BRI</legend>
+						<p>
+							<label for="l2protocol">Protocole de couche 2: </label>
+							<select name="l2protocol" id="l2protocol" class="form-control">
+								<option value="pp" selected>pp</option>
+								<option value="pmp">pmp</option>
+							</select>
+						</p>
+						<p>
+							<label for="incallform">Format d'appel entrant: </label>
+							<select name="incallform" id="incallform" class="form-control">
+								<option value="4">4 chiffres</option>
+								<option value="9">9 chiffres</option>
+								<option value="10" selected>10 chiffres</option>
+							</select>
+						</p>
+						<p>
+							<label for="outcallform">Format d'appel sortant (CLIP): </label>
+							<select name="outcallform" id="outcallform" class="form-control">
+								<option value="9">9 chiffres</option>
+								<option value="10" selected>10 chiffres</option>
+							</select>
+						</p>
+					</fieldset>
+					<fieldset>
+						<legend>Paramètres PRI</legend>
+						<p>
+							<label for="canauxt2">Nombre de canaux T2: </label>
+							<input type="text" name="canauxt2" id="canauxt2" class="form-control" placeholder="Max 15 ou 30..." />
+						</p>
 					</fieldset>
 					<p>
 						<center>
